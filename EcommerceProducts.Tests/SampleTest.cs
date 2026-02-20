666b1e27502596c72dc05927b948170aabe8c065
Leonardo Anjos <leodrums11@gmail.com>
2026-02-20 16:59:52 -0300

    initial tests structure

---
+++ EcommerceProducts.Tests/SampleTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace EcommerceProducts.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
